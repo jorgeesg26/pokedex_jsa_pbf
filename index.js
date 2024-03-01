79f7f0cd79cc1e9126a1f4db453d6bfbb0359485
jorgeesg26 <145496908+jorgeesg26@users.noreply.github.com>
2024-03-01 23:15:48 +0100

    actualizacion
    ole lokete q termino la pagina we

---
+++ index.js
@@ -0,0 +1,112 @@
+import { traducirTipo as _traducirTipo } from "./traducirTipo.js";
+const buscador = document.getElementById('barraBusqueda'); 
+const listaPokemon = document.getElementById('lista-pokemon');
+const arrayPokemonTodos = [];
+
+pokemon();
+
+async function pokemon() {
+    await obtenerPokemons();
+    drawPokemons(arrayPokemonTodos);
+} 
+
+async function obtenerPokemons() {
+    for (let i = 1; i <= 151; i++) {
+        let pokemon = await getData(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        arrayPokemonTodos.push(pokemon);
+    }
+}
+
+function drawPokemons(pokemons) {
+    listaPokemon.innerHTML = '';
+    for(const pokemon of pokemons){
+        const pokemonCard = document.createElement('div');
+        pokemonCard.classList.add('pokemonCard');
+        pokemonCard.innerHTML += `<a href="vistainterna.html?id=${pokemon.id}">
+        <img style="height=200px; display: grid; margin: 0 auto;" src="${pokemon.sprites.other["official-artwork"].front_default}">
+        <div  style = "text-align: center;">${pokemon.name}</div>
+        </a>`;   
+        pokemonCard.innerHTML += '<div style = "text-align: center;" class="'+ cambiarColor(pokemon.types[0].type.name) +'">' + _traducirTipo(pokemon.types[0].type.name) +'</div>';
+        if (pokemon.types.length > 1) {
+            pokemonCard.innerHTML += '<div style = "text-align: center;" class="'+ cambiarColor(pokemon.types[1].type.name) +'">' + _traducirTipo(pokemon.types[1].type.name) +'</div>';
+        }
+
+        listaPokemon.appendChild(pokemonCard);
+    }
+}
+
+async function getData(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+}
+
+function cambiarColor(tipo) {
+    switch (tipo) {
+        case 'grass':
+            return 'pokemonCardPlanta';
+            break;
+        case 'bug':
+            return 'pokemonCardBicho';
+            break;
+        case 'fire':
+            return 'pokemonCardFuego';
+            break;
+        case 'water':
+            return 'pokemonCardAgua';
+            break;
+        case 'ice':
+            return 'pokemonCardHielo';
+            break;
+        case 'electric':
+            return 'pokemonCardElectrico';
+            break;
+        case 'ground':
+            return 'pokemonCardTierra';
+            break;
+        case 'rock':
+            return 'pokemonCardRoca';
+            break;
+        case 'fighting':
+            return 'pokemonCardLucha';
+            break;
+        case 'steel':
+            return 'pokemonCardAcero';
+            break;
+        case 'psychic':
+            return 'pokemonCardPsiquico';
+            break;
+        case 'poison':
+            return 'pokemonCardVeneno';
+            break;
+        case 'fairy':
+            return 'pokemonCardHada';
+            break;
+        case 'normal':
+            return 'pokemonCardNormal';
+            break;
+        case 'dragon':
+            return 'pokemonCardDragon';
+            break;
+        case 'ghost':
+            return 'pokemonCardFantasma';
+            break;
+        case 'flying':
+            return 'pokemonCardVolador';
+            break;
+        default:
+            break;
+    }
+}
+
+buscador.addEventListener('input', () => {
+    const searchText = buscador.value.toLowerCase().trim();
+    const pokemonEncontrados = arrayPokemonTodos.filter(pokemon => pokemon.name.includes(searchText) || pokemon.id.toString().includes(searchText));
+    console.log('Pokémon filtrados:', pokemonEncontrados);
+    drawPokemons(pokemonEncontrados);
+}
+);
+const botonModoClaroOscuro = document.getElementById("modo-claro-oscuro");
+botonModoClaroOscuro.addEventListener("click", () => {
+document.body.classList.toggle("modo-oscuro");
+}); 
---
+++ vistainterna.js
@@ -0,0 +1,67 @@
+    import { traducirTipo as _traducirTipo } from "./traducirTipo.js";
+
+    const pokemonURL = new URLSearchParams(window.location.search);
+    const id = pokemonURL.get('id');
+
+    getPokemonData(id);
+    const listaPokemon = document.getElementById("lista-pokemon");
+    async function getData(url) {
+        const response = await fetch (url);
+        const json = await response.text();
+        return JSON.parse(json);
+    }
+    
+    async function getPokemonData(id) {   
+        const obj = await getData(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        listaPokemon.classList.add('flex');
+        listaPokemon.innerHTML += '<div class = "centrar">';
+        listaPokemon.innerHTML +=  '<div id ="nombre" style = "text-align: center;font-size:50px">' + obj.name+ '<div>';
+        listaPokemon.innerHTML +=  '<div style = "text-align: center;opacity:0.4">N°' + obj.id+ '<div>';
+        listaPokemon.innerHTML += '<img src=' + obj.sprites.other.showdown.front_default + ' style="height=200px; display: grid; margin: 0 auto;">';
+        listaPokemon.innerHTML +=  '<div style = "text-align: center;">' + obj.weight/10+ 'Kg <div>';
+        listaPokemon.innerHTML +=  '<div style = "text-align: center;">' + obj.height/10+ 'm<div>';
+        if (obj.types.length>1) {
+            listaPokemon.innerHTML += '<div style = "text-align: center;">' + _traducirTipo(obj.types[0].type.name) + '/' + _traducirTipo(obj.types[1].type.name) +'<div>';
+        } else {
+            listaPokemon.innerHTML += '<div style = "text-align: center;">' + _traducirTipo(obj.types[0].type.name) + '<div>'
+        }
+        listaPokemon.innerHTML += '<br>';
+        listaPokemon.innerHTML += `
+        <div style = "text-align: center;"><h1>Estadísticas</h1></div>
+                <div style = "text-align: center;">Vida</div>
+                <div style = "text-align: center;">
+                <progress value="${obj.stats[0].base_stat}" max="255"></progress>
+                ${obj.stats[0].base_stat}
+                </div>
+                <div style = "text-align: center;">Ataque</div>
+                <div style = "text-align: center;">
+                <progress value="${obj.stats[1].base_stat}" max="255"></progress>
+                ${obj.stats[1].base_stat}
+                </div>
+                <div style = "text-align: center;">Defensa</div>
+                <div style = "text-align: center;">
+                <progress value="${obj.stats[2].base_stat}" max="255"></progress>
+                    ${obj.stats[2].base_stat}
+                </div> 
+                <div style = "text-align: center;">Ataque Especial</div>
+                <div style = "text-align: center;">
+                <progress value="${obj.stats[3].base_stat}" max="255"></progress>
+                    ${obj.stats[3].base_stat}
+                </div> 
+                <div style = "text-align: center;">Defensa Especial</div>
+                <div style = "text-align: center;">
+                    <progress value="${obj.stats[4].base_stat}" max="255"></progress>
+                    ${obj.stats[4].base_stat}
+                </div>
+            <div style = "text-align: center;">Velocidad</div>
+            <div style = "text-align: center;">
+            <progress value="${obj.stats[5].base_stat}" max="255"></progress>
+                ${obj.stats[5].base_stat}`; 
+    listaPokemon.innerHTML +='</div>';
+    
+}    
+    const botonModoClaroOscuro = document.getElementById("modo-claro-oscuro");
+    botonModoClaroOscuro.addEventListener("click", () => {
+    document.body.classList.toggle("modo-oscuro");
+    });
+        ;  
